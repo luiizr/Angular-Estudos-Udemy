0b14f6a3e60cfec1ae93b076430677f17d0349f7
luiizr <luizroberto312005@gmail.com>
2025-01-30 08:36:28 -0300

    Bug Fix

--- angular/src/app/components/template/nav/nav.component.ts
+++ angular/src/app/components/template/nav/nav.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+// import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar'
+
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, MatToolbarModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
